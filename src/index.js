f0b980a9942b7d3c8fd59f421d1b7c38f5f1bd24
sri0711 <m.sathish0711@gmail.com>
2021-02-20 11:30:34 +0530

    for class

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 8000;
 const app = express();
 const views = path.join(__dirname, '../views/hbs_files');
 const template = path.join(__dirname, '../views/template');
-domain = require('../routers/domain');
+const domain1 = require('../routers/domain');
 
 // inialiginzing directories
 app.set('view engine', 'hbs');
 app.set('views', views);
 app.use(express.static(path.join(__dirname, '../public')));
 hbs.registerPartials(template);
-app.use(domain);
+app.use(domain1);
 
 // server settings
 app.listen(
